c56de2b173c5153dc40da5dc51bff0c3930fa223
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-18 03:02:16 +0500

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+);

750061aae354f488e0217662fa3a1d3d201a9a9e
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-18 03:08:13 +0500

    Fix linters

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
-    def index
-        @random_message = Message.order(Arel.sql('RANDOM()')).first
-        render json: {greeting: @random_message.greeting}
-    end
+  def index
+    @random_message = Message.order(Arel.sql('RANDOM()')).first
+    render json: { greeting: @random_message.greeting }
+  end
 end

535416c3dd3856dbbbb4b333f560b08160a3ba06
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-07-18 22:01:33 +0500

    Remove console logs

--- app/javascript/greetingSlice.js
+++ app/javascript/greetingSlice.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 export const randomGreeting = createAsyncThunk('greeting/getRandomGreeting', async() => {
     try {
         const response = await axios.get('/api/messages');
-        console.log(response)
         return response.data.greeting;
       } catch (error) {
         throw error.response.data.error;
@@ -27,7 +26,6 @@ const greetingSlice = createSlice({
         })
         .addCase(randomGreeting.fulfilled, (state, action) => {
           state.loading = false;
-          console.log(action)
           state.greeting = action.payload;
         })
         .addCase(randomGreeting.rejected, (state, action) => {
